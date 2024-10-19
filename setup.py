8ad6f11f36522daf3b3c267322ab006c37edddbd
homostultus39 <145347898+homostultus39@users.noreply.github.com>
2024-10-19 12:36:03 +0300

    added main files of library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiogram_calendar",
+    version="0.5.0",
+    packages=find_packages(),
+    install_requires=[],      
+    description="A calendar widget library for aiogram",
+    author="homostultus39",
+    author_email="homostultus39@gmail.com",
+    url="https://github.com/homostultus39/widget_calendar",
+)
